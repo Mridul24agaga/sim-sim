af66303e6bb76ab51ccdc4953cf27c7a62362d8c
Mridul24agaga <101731772+Mridul24agaga@users.noreply.github.com>
2023-12-29 20:44:06 +0530

    Update typeWriter.js

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,7 +1,7 @@
 
 let i = 0;
-let text1 = "Hey! Miss Qiu.";
-let text2 = "Today is the Big Day."
+let text1 = "Hey Gursimar! do you know why i made this website? I made this website to tell you how much you mean to me. You are the best person i have ever met and i cant imagine my life without you for real no lies. you are the best, cutest, sweetest, kindest and pretty! .";
+let text2 = "You know i always find ways to make you happy because your happinesss is the only thing i want to see and yeah, you are myy first priority forever<3 i know this year would be the best year for you and yeah ur a cutiepie<3."
 let speed = 100;
 
 function typeWriter(text, para){
